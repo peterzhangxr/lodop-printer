b6623faf1d25888cc52c6b139d4feb8d026640f0
Peter Zhang <peter.zhang.henan@gmail.com>
2020-07-30 14:14:06 +0800

    lodop

--- src/index.js
+++ src/index.js
@@ -17,16 +17,16 @@ const draw = (LODOP, options = {}) => {
   LODOP.SET_PRINT_PAGESIZE(3, 760, 150, "");
   LODOP.SET_PRINT_MODE("POS_BASEON_PAPER", true);
   LODOP.SET_PRINT_STYLE("FontSize", 24);
-  LODOP.ADD_PRINT_TEXT(10, 0, 580, 40,"省的多");
+  LODOP.ADD_PRINT_TEXT(0, 0, 580, 40,"省的多");
   LODOP.SET_PRINT_STYLE("FontSize", 12);
-  LODOP.ADD_PRINT_TEXT(60, 0, 580, 20,"下单时间：" + orderTime);  
-  LODOP.ADD_PRINT_TEXT(80, 0, 580, 20,"支付金额：" + money);  
-  LODOP.ADD_PRINT_TEXT(100, 0, 580, 20,"收货人：" + receiver);  
-  LODOP.ADD_PRINT_TEXT(120, 0, 580, 20,"电话：" + phone);  
-  LODOP.ADD_PRINT_TEXT(140, 0, 580, 20,"自提点：" + siteName);  
-  LODOP.ADD_PRINT_TEXT(160, 0, 580, 20,"自提点电话：" + sitePhone);  
-  LODOP.ADD_PRINT_LINE(190, 0, 190, 580)
-  LODOP.ADD_PRINT_TEXT(200, 60, 580, 20,"----购物清单----");
+  LODOP.ADD_PRINT_TEXT(40, 0, 580, 20,"下单时间：" + orderTime);  
+  LODOP.ADD_PRINT_TEXT(60, 0, 580, 20,"支付金额：" + money);  
+  LODOP.ADD_PRINT_TEXT(80, 0, 580, 20,"收货人：" + receiver);  
+  LODOP.ADD_PRINT_TEXT(100, 0, 580, 20,"电话：" + phone);  
+  LODOP.ADD_PRINT_TEXT(120, 0, 580, 20,"自提点：" + siteName);  
+  LODOP.ADD_PRINT_TEXT(140, 0, 580, 20,"自提点电话：" + sitePhone);  
+  LODOP.ADD_PRINT_LINE(170, 0, 170, 580)
+  LODOP.ADD_PRINT_TEXT(180, 60, 580, 20,"----购物清单----");
 
 
   for(let [index, item] of shoppingList.entries()) {
@@ -36,7 +36,7 @@ const draw = (LODOP, options = {}) => {
       money = 0 //金额
     } = item
     
-    let top = 230 + index * 70
+    let top = 210 + index * 70
     let top2 = top + 10
     LODOP.ADD_PRINT_LINE(top, 0, top, 580, 2)
     LODOP.ADD_PRINT_TEXT(top2, 0, 350, 70, name);  
@@ -44,7 +44,7 @@ const draw = (LODOP, options = {}) => {
     LODOP.ADD_PRINT_TEXT(top2, 190, 150, 70, money);
   }
 
-  let start = 230 + shoppingList.length *  70;
+  let start = 210 + shoppingList.length *  70;
   LODOP.ADD_PRINT_LINE(start, 0, start, 580)
   LODOP.SET_PRINT_STYLE("Italic", 1);
   LODOP.ADD_PRINT_TEXT(start + 20, 89, 580, 20, "省的多");
